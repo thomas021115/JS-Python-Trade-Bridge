78fd3a63f17ea5591337062882ecff7af40b4ab5
Thomas <thomas021115@gmail.com>
2026-01-02 14:59:41 +0800

    import pages to router index.ts

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,3 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AiAnalysis from "@/pages/AiAnalysis.vue"
+import Portfolio from "@/pages/Portfolio.vue";
+import Todo from "@/pages/Todo.vue"
+
+export const router = createRouter({
+history: createWebHistory(),
+routes: [
+
+]
+});
