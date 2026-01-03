f76862903a258a002cab1fec29690ce81268ccb1
Thomas <thomas021115@gmail.com>
2026-01-03 15:17:58 +0800

    add RouterLink aianalysis

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,6 +3,7 @@
    <nav style="display:flex; gap: 12px; margin-bottom: 10px;">
     <RouterLink to="/todo">Todo</RouterLink>
     <RouterLink to="/portfolio">Portfolio</RouterLink>
+    <RouterLink to="/aianalysis">AiAnalysis</RouterLink>
    </nav>
    <RouterView />
   </div>
