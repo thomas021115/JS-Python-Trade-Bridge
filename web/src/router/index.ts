ed3e11a37b2dd34c3196e0fe13a598ff24ef2808
Thomas <thomas021115@gmail.com>
2026-01-03 14:59:11 +0800

    add todo and portfolio routes

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -7,6 +7,7 @@ import Todo from "@/pages/Todo.vue"
 export const router = createRouter({
 history: createWebHistory(),
 routes: [
-
-]
+    { path: "/todo", component: Todo},
+    { path: "/portfolio", component: Portfolio},
+],
 });
