1336ef71582694057d6c8943d8b3b5ba6322e873
Thomas <thomas021115@gmail.com>
2026-02-03 15:30:26 +0800

    feat(layout): add PageShell component

---
+++ web/src/components/layout/PageShell.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type Props = {
+  /** 外層額外 class*/
+  outerClass?: string;
+  /** 內容容器 class*/
+  containerClass?: string;
+};
+
+withDefaults(defineProps<Props>(), {
+  outerClass: "py-10",
+  containerClass: "px-6 w-full max-w-xl mx-auto",
+});
+</script>
