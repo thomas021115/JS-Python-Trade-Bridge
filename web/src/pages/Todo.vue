931c0121e09f33df29d5fdd07d932c6d1ebd1ed1
thomas <thomas021115@gmail.com>
2026-02-10 14:20:30 +0800

    fix: move report store to correct directory

--- web/src/pages/Todo.vue
+++ web/src/pages/Todo.vue
@@ -39,7 +39,7 @@ function removeTodo(id: number){
 
 <template>
 	<div>
-		<button @click="count = count + 1">{{ count }}</button>
+
 		<h1>開發筆記 / Todo</h1>
 		<p>用於練習 Vue 組件化與 Pinia 狀態管理 [3]。</p>
 	</div>
