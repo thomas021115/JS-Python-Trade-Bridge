316faf335adfdcfefe752253b473313c9c42f958
4toast <g650azy@gmail.com>
2026-02-10 16:36:20 +0800

    feat(layout): implement global navigation header
    - 在components/layout底下建立Header.vue
    - 在App.vue中引入Header

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Sidebar from '@/components/layout/Sidebar.vue';
+import Header from '@/components/layout/Header.vue';
 </script>
 
 <template>
 	<div class="flex h-screen bg-slate-50 overflow-hidden">
 		<Sidebar class="flex-shrink-0" />
-
+		<Header />
 		<div class="flex-1 flex flex-col min-w-0">
 			<main class="flex-1 overflow-auto p-6">
 				<router-view></router-view>
---
+++ web/src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Search } from 'lucide-vue-next';
+</script>
+
+<template>
+	<header
+		class="flex bg-white ml-0.5 border-slate-200 h-16 items-center justify-between px-6 sticky top-0 z-20"
+	>
+		<div class="flex items-center ml-10">
+			<div class="relative w-64">
+				<span class="absolute inset-y-0 left-2 flex items-center"
+					><Search class="w-4 h-4 text-slate-400"
+				/></span>
+				<input
+					type="text"
+					class="block w-full border border-slate-300 rounded-xl pl-8 py-2 text-sm"
+					placeholder="搜尋代碼/名稱"
+				/>
+			</div>
+		</div>
+
+		<div class="flex items-center gap-4">Right Zone</div>
+	</header>
+</template>
