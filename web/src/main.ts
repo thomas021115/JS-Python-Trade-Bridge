e17b86110ee0b56400ea6c6c893b5d0357da127c
Thomas <thomas021115@gmail.com>
2026-02-11 14:41:36 +0800

    add basic Todo page template & setup Pinia in main.ts

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,5 +4,6 @@ import { router } from "./router";
 import "./style.css";
 import Vue3Toastify from "vue3-toastify";
 import "vue3-toastify/dist/index.css" //node_modules裡的vue3-toastify
+import { createPinia } from "pinia";
 
-createApp(App).use(router).use(Vue3Toastify, {position: "top-right",autoClose: 2000,}).mount("#app"); //引入並預設Vue3Toastify 位置
+createApp(App).use(createPinia()).use(router).use(Vue3Toastify, {position: "top-right",autoClose: 2000,}).mount("#app"); //引入並預設Vue3Toastify 位置
